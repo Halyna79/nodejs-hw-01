bbe3f2173b8cc90ce803c7da5fcdf69c3f5e9601
halyna <galinabandurka7@gmail.com>
2025-05-31 19:45:00 +0200

    updatenode

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+
+const generateContacts = async (number) => {
+    const oldContacts = await readContacts();
+    const newContacts = Array.from({ length: number }, () => createFakeContact());
+    const updatedContacts = oldContacts.concat(newContacts);
+    await writeContacts(updatedContacts);
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { writeFile } from 'fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    try {
+        await writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2), 'utf-8');
+    } catch (error) {
+        console.error('Error writing the file:', error.message);
+        return [];
+    }
+};
